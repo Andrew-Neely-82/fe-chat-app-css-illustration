a143323dab76f2609223dae467f07d11d4447d62
Andrew-Neely-82 <andrew.neely82@gmail.com>
2024-07-15 15:29:46 -0500

    added more advanced chat animation

--- src/components/phone/Body.jsx
+++ src/components/phone/Body.jsx
@@ -1,29 +1,85 @@
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import { ReceivedMessage } from "./body/ReceivedMessage";
 import { ServicesMessage } from "./body/ServicesMessage";
 import { SentMessage } from "./body/SentMessage";
 import { SentImages } from "./body/SentImages";
-import PropTypes from "prop-types";
 
 const Body = ({ phoneData }) => {
+  const [currentStep, setCurrentStep] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (currentStep < 6) {
+      // Show loading bubble
+      const loadingTimer = setTimeout(() => {
+        setIsLoading(true);
+      }, 2000);
+
+      // Show actual message
+      const messageTimer = setTimeout(() => {
+        setIsLoading(false);
+        setCurrentStep((prevStep) => prevStep + 1);
+      }, 3000);
+
+      return () => {
+        clearTimeout(loadingTimer);
+        clearTimeout(messageTimer);
+      };
+    }
+  }, [currentStep]);
+
+  const Loading = ({ style }) => {
+    return (
+      <div className="stage" style={style}>
+        <div className="dot-pulse"></div>
+      </div>
+    );
+  };
+
+  Loading.propTypes = {
+    style: PropTypes.object.isRequired,
+  };
+
+  const loadingStyle = {
+    display: "flex",
+    height: "2rem",
+    margin: "1rem 0px 0px 0",
+    alignContent: "center",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    width: "5rem",
+    borderRadius: "0.75rem",
+    padding: "0",
+  };
+
   return (
     <div className="phone-body">
       <div className="received-messages-container">
-        <ReceivedMessage className={"slide-top1"} message={phoneData[0].conversation.sender[0][0]} />
-        <ReceivedMessage className={"slide-top2"} message={phoneData[0].conversation.sender[0][1]} />
+        {currentStep === 0 && isLoading ? <Loading style={loadingStyle} /> : currentStep >= 1 ? <ReceivedMessage className={"slide-top1"} message={phoneData[0].conversation.sender[0][0]} /> : null}
+
+        {currentStep === 1 && isLoading ? <Loading style={loadingStyle} /> : currentStep >= 2 ? <ReceivedMessage className={"slide-top2"} message={phoneData[0].conversation.sender[0][1]} /> : null}
       </div>
       <div className="sent-messages-container">
-        <SentImages data={phoneData} />
-        <SentMessage className={"slide-top3"} message={phoneData[0].conversation.recipient[0]} />
-        <SentMessage className={`slide-top4`} message={phoneData[0].conversation.recipient[1]} />
+        {currentStep === 2 && isLoading ? (
+          <Loading style={loadingStyle} />
+        ) : currentStep >= 3 ? (
+          <>
+            <SentImages data={phoneData} />
+            <SentMessage className={"slide-top3"} message={phoneData[0].conversation.recipient[0]} />
+          </>
+        ) : null}
+
+        {currentStep === 3 && isLoading ? <Loading style={loadingStyle} /> : currentStep >= 4 ? <SentMessage className={"slide-top4"} message={phoneData[0].conversation.recipient[1]} /> : null}
       </div>
-      <div className="received-messages-container">
-        <ReceivedMessage className={"slide-top5"} message={phoneData[0].conversation.sender[1]} />
-      </div>
-      <ServicesMessage data={phoneData[0]} />
+      <div className="received-messages-container">{currentStep === 4 && isLoading ? <Loading style={loadingStyle} /> : currentStep >= 5 ? <ReceivedMessage className={"slide-top5"} message={phoneData[0].conversation.sender[1]} /> : null}</div>
+      {currentStep === 5 && isLoading ? <Loading style={loadingStyle} /> : currentStep >= 6 ? <ServicesMessage data={phoneData[0]} /> : null}
     </div>
   );
 };
 
-Body.propTypes = { phoneData: PropTypes.any.isRequired };
+Body.propTypes = {
+  phoneData: PropTypes.array.isRequired,
+};
 
 export default Body;
